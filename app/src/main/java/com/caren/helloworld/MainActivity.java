197dad3efae41cfd02ebf111431c276cdf45dab4
calren <calren24@gmail.com>
2019-02-25 21:08:32 -0800

    first commit

---
+++ app/src/main/java/com/caren/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.caren.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void printOutUsernames() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Log.i("MainActivity", users.get(i).name);
+        }
+    }
+
+    public class User {
+        String name;
+    }
+}
